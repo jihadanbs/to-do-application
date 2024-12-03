d3f4da4aed93e144f3e31ba25aff68fc25d126be
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2024-12-03 16:01:47 +0700

    [ADD] membuat file deploy serta mengatur configurasi hardhat.config

--- contracts/TaskToDo.sol
+++ contracts/TaskToDo.sol
@@ -1,5 +1,5 @@
-// SPDX-Licence-Identifier: GPL-3.0
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.27;
 
 contract TaskToDo {
     enum TaskStatus {Pending, Finished}
---
+++ ignition/modules/Deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+  // Mendapatkan contract factory
+  const TaskToDo = await hre.ethers.getContractFactory("TaskToDo");
+  
+  // Deploy kontrak
+  const taskToDo = await TaskToDo.deploy();
+  
+  // Menunggu kontrak di-deploy
+  await taskToDo.deploymentTransaction().wait(1);
+  
+  // Mendapatkan alamat kontrak
+  const contractAddress = await taskToDo.getAddress();
+  
+  console.log("Contract deployed to:", contractAddress);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
